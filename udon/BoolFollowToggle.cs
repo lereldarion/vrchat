22f86eed94dfae2e66e7fae870be5ac0836b716c
Lereldarion <131812197+lereldarion@users.noreply.github.com>
2024-02-25 01:47:49 +0100

    Udon utils : update bool field to follow toggle

---
+++ udon/BoolFollowToggle.cs
@@ -0,0 +1,22 @@
+﻿
+using UdonSharp;
+using UnityEngine.UI;
+using VRC.SDKBase;
+using VRC.Udon;
+
+// Attach to a UI Toggle, an make bool variable
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+public class BoolFollowToggle : UdonSharpBehaviour {
+    public UdonBehaviour update_target;
+    private Toggle self_toggle;
+
+    void Start() {
+        self_toggle = GetComponent<Toggle>();
+        OnToggleValueChanged(); // Force sync
+    }
+
+    // Link toggle OnValueChanged to this CustomEvent
+    public void OnToggleValueChanged() {
+        update_target.SetProgramVariable<bool>(name, self_toggle.isOn);
+    }
+}
